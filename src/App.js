cdfe38ea6c47690b9736ca7a6b86c64cee84849d
User <y55mun33@gmail.com>
2023-01-31 22:32:56 +0900

    컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -49,8 +49,20 @@ function App() {
         <p>2월 17일 발행</p>
         <hr/>
       </div>
+      <Modal></Modal>
+
     </div>
   );
+}
+
+function Modal() {
+  return(
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
